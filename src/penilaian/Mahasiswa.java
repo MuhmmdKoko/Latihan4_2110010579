932beaac963e63d0ea3de9c352720e0642e4389d
MuhmmdKoko <someanncat@gmail.com>
2023-06-13 09:25:03 +0800

    Membuat dan melengkapi class Mahasiswa dan NilaiAkhir

---
+++ src/penilaian/Mahasiswa.java
@@ -0,0 +1,25 @@
+package penilaian;
+
+public class Mahasiswa {
+    //membuat variabel
+    String nama, npm;
+    
+    //membuat getter dan setter
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+    
+    
+}
---
+++ src/penilaian/NilaiAkhir.java
@@ -0,0 +1,38 @@
+package penilaian;
+
+public class NilaiAkhir extends Mahasiswa {
+    //membuat variabel
+    int uts, uas, tugas;
+
+    public NilaiAkhir(int uts, int uas, int tugas) {
+        this.uts = uts;
+        this.uas = uas;
+        this.tugas = tugas;
+    }
+
+    double hitungNilaiAkhir(){
+        return 0.3*uts + 0.3*uas + 0.4*tugas;
+    }
+    
+    @Override
+    public void setNpm(String npm) {
+        super.setNpm(npm); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNpm() {
+        return super.getNpm(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNama(String nama) {
+        super.setNama(nama); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNama() {
+        return super.getNama(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
